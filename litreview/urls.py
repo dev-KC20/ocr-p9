887a11dbab2acceb170eca8a8ac16d3bee3fefd5
dev@KC <dev@kitconseil.fr>
2022-04-13 20:50:40 +0200

    chore: align urls

--- litreview/urls.py
+++ litreview/urls.py
@@ -16,73 +16,13 @@ Including another URLconf
 from django.urls import path, include
 from django.contrib import admin
 
-import review.views
-from review.views import (
-    HomeView,
-    TicketDetailView,
-    TicketCreateView,
-    TicketUpdateView,
-    ReviewCreateView,
-    UserSubscribeView,
-    UserSubscriptionView,
-    UserUnsubscribeView,
-    UserFollowersView,
-    UserSubscriptionsView,
-)
-
 # media
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("review/feed/", review.views.feed, name="feed"),
-    path("home/", HomeView.as_view(template_name="review/home.html"), name="home"),
-    path(
-        "review/create-ticket/",
-        TicketCreateView.as_view(),
-        name="create_ticket",
-    ),
-    path(
-        "review/create-review/",
-        ReviewCreateView.as_view(),
-        name="create_review",
-    ),
-    path(
-        "review/<int:pk>/update/",
-        TicketUpdateView.as_view(),
-        name="update_ticket",
-    ),
-    path(
-        "review/<int:pk>/view/",
-        TicketDetailView.as_view(),
-        name="show_ticket",
-    ),
-    path(
-        "review/subscribe/",
-        UserSubscribeView.as_view(),
-        name="subscribe_user",
-    ),
-    path(
-        "review/subscription/",
-        UserSubscriptionView.as_view(),
-        name="subscription",
-    ),
-    path(
-        "review/subscriptions/",
-        UserSubscriptionsView.as_view(),
-        name="subscriptions",
-    ),
-    path(
-        "review/<int:pk>/unsubscribe/",
-        UserUnsubscribeView.as_view(),
-        name="unsubscribe_user",
-    ),
-    path(
-        "review/followers/",
-        UserFollowersView.as_view(),
-        name="followers",
-    ),
+    path('', include('review.urls')),
 ]
 # lowtech file storage solution for academic purpose & money wise
 if settings.DEBUG:
